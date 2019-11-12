5a20ed7c75194ab03f80ab82b37600a1167caba1
Yonger <32595313+fanfeng07@users.noreply.github.com>
2019-11-12 13:06:06 +0100

    delete files

--- test.py
+++
@@ -1,8 +0,0 @@
-with open('baseline/prepared_data/dict.de','r') as f: #bpe/new_dict.de
-    i = 0
-    for line in f.readlines():
-        if i <=15:
-            word = line.rstrip().rsplit(' ',1)
-            print(word)
-        i+=1
-        
